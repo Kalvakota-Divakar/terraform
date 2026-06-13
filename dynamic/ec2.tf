aa20d1d6a84c7a3fcd56722ffb778e9d4699ad49
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-13 22:20:42 +0530

    updated ec2

--- dynamic/ec2.tf
+++ dynamic/ec2.tf
@@ -1,12 +1,4 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.micro"
-  vpc_security_group_ids =[aws_security_group.allow_tls.id]
-  tags = {
-    Name = "Terraform"
-    Project= "roboshop"
-  }
-}
+
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow-all-terraform" # this is for aws account
