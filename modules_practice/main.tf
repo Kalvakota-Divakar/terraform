e89d7e2707eee4cc9233e5156bd561a811505336
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-14 13:29:32 +0530

    modules

--- modules_practice/main.tf
+++ modules_practice/main.tf
@@ -1,11 +1,3 @@
-variable "ami_id" {
-    description = " The AMI ID to use for the EC2 instance"
-}
-
-variable "instance_type" {
-    description = " The type of EC2 instance to create"
-}
-
 
 provider "aws" {
   region = "us-east-1"
