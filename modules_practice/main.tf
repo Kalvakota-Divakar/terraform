dfda268bf38cf6a14162f4ae59a141152b03d1e8
Kalvakota Divakar <kalvakotadivakar@gmail.com>
2026-06-14 13:16:39 +0530

    modules

--- modules_practice/main.tf
+++ modules_practice/main.tf
@@ -1,7 +1,3 @@
-
-# module block to create an EC2 instance using the ec2-instance module
-module "ec2_instance" {
-    source = "./modules/ec2-instance" # Path to the EC2 instance module
-    ami_id = "ami-0220d79f3f480ecf5" # Replace with a valid AMI ID for your region
-    instance_type = "t3.micro"
+provider "aws" {
+  region = us-east-1
 }
